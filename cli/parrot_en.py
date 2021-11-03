2a92b4a3b712e9303f17f5cc73b6f62e6508b997
ftnext <takuyafjp+develop@gmail.com>
2021-11-03 22:52:22 +0900

    [feat] First example: chatbot without training (close #3)

---
+++ cli/parrot_en.py
@@ -0,0 +1,12 @@
+from chatterbot import ChatBot
+
+if __name__ == "__main__":
+    chatbot = ChatBot("Parrot bot")
+
+    while True:
+        try:
+            user_input = input("You: ")
+            response = chatbot.get_response(user_input)
+            print(f"Bot: {response}")
+        except (KeyboardInterrupt, EOFError, SystemExit):
+            break
