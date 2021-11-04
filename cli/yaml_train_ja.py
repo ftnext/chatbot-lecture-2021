ac4fe75396cce0a79abe7b9870f991253267ca37
ftnext <takuyafjp+develop@gmail.com>
2021-11-05 01:45:54 +0900

    [feat] Train chatbot from yaml (close #5)

---
+++ cli/yaml_train_ja.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+from chatterbot import ChatBot
+from chatterbot.languages import JPN
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+
+class CustomJPN(JPN):
+    ISO_639_1 = "ja_core_news_sm"
+
+
+if __name__ == "__main__":
+    cli_dir_path = Path(__file__).parent
+    data_yml_path = cli_dir_path / "greetings_ja.yml"
+
+    chatbot = ChatBot("Training from yaml bot", tagger_language=CustomJPN)
+
+    trainer = ChatterBotCorpusTrainer(chatbot)
+    trainer.train(str(data_yml_path))
+
+    while True:
+        try:
+            user_input = input("You: ")
+            response = chatbot.get_response(user_input)
+            print(f"Bot: {response}")
+        except (KeyboardInterrupt, EOFError, SystemExit):
+            break
