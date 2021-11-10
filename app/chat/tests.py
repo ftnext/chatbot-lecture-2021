20e63315249209c1481902d343dfca81ddae8c3e
ftnext <takuyafjp+develop@gmail.com>
2021-11-10 21:33:17 +0900

    [test] Verify template with rename classes

--- app/chat/tests.py
+++ app/chat/tests.py
@@ -4,12 +4,19 @@ from django.urls import resolve
 from chat import views
 
 
-class HomePageViewTest(TestCase):
-    def test_home_returns_200(self):
+class HomePageTestCase(TestCase):
+    def test_home_returns_200_and_expected_title(self):
         response = self.client.get("/")
 
-        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Talk with chatbot", status_code=200)
 
+    def test_home_page_uses_expected_template(self):
+        response = self.client.get("/")
+
+        self.assertTemplateUsed(response, "chat/home.html")
+
+
+class HomeViewTestCase(TestCase):
     def test_should_resolve_home(self):
         found = resolve("/")
 
