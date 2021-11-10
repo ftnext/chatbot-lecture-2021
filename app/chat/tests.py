66b0f5f16e33e7e3bc3bb117f0f27e491457b5ac
ftnext <takuyafjp+develop@gmail.com>
2021-11-10 21:15:27 +0900

    [test] Request / by client

--- app/chat/tests.py
+++ app/chat/tests.py
@@ -1,13 +1,8 @@
-from django.http import HttpRequest
 from django.test import TestCase
-
-from chat.views import home
 
 
 class HomePageViewTest(TestCase):
     def test_home_returns_200(self):
-        request = HttpRequest()
-
-        response = home(request)
+        response = self.client.get("/")
 
         self.assertEqual(response.status_code, 200)
